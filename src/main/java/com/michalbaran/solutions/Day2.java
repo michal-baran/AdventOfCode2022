ad51c94f63a1acf176464dea3765c1ba4ffbd4cb
Michal Baran <michal.baran.dev@gmail.com>
2022-12-03 02:52:05 +0100

    Day 2 - Part One (template)

---
+++ src/main/java/com/michalbaran/solutions/Day2.java
@@ -0,0 +1,22 @@
+package com.michalbaran.solutions;
+
+import com.michalbaran.utils.StaticUtils;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class Day2 {
+    private static Logger logger = Logger.getLogger(Day1.class.getName());
+
+    public Day2(String filename) {
+        List<String> inputList = StaticUtils.inputFileToStringList(filename);
+    }
+
+    public int PartOne() {
+        return 0;
+    }
+
+    public int PartTwo() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/michalbaran/solutions/Day2Test.java
@@ -0,0 +1,22 @@
+package com.michalbaran.solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day2Test {
+    static String example = "/src/test/resources/day2_example.txt";
+    static String input = "/src/main/resources/day2.txt";
+
+    @Test
+    public void testPartOne() {
+        assertEquals(15, new Day2(example).PartOne());
+       // assertEquals(71023, new Day2(input).PartOne());
+    }
+
+    @Test
+    public void testPartTwo() {
+        //assertEquals(45000, new Day2(example).PartTwo());
+        //assertEquals(206289, new Day2(input).PartTwo());
+    }
+}
