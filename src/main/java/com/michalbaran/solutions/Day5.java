be48dabba8ad8f9d41e809794b5f8d56197ec348
Michal Baran <michal.baran.dev@gmail.com>
2022-12-05 21:17:31 +0100

    Day 5 - Template

---
+++ src/main/java/com/michalbaran/solutions/Day5.java
@@ -0,0 +1,18 @@
+package com.michalbaran.solutions;
+
+public class Day5 extends Day{
+
+    public Day5(String filename) {
+        super(filename);
+    }
+
+    @Override
+    public int PartOne() {
+        return 0;
+    }
+
+    @Override
+    public int PartTwo() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/michalbaran/solutions/Day5Test.java
@@ -0,0 +1,20 @@
+package com.michalbaran.solutions;
+
+import org.junit.jupiter.api.Test;
+
+class Day5Test {
+    String example = "/src/test/resources/day5_example.txt";
+    String input = "/src/main/resources/day5.txt";
+
+    @Test
+    public void testPartOne() {
+//        assertEquals(2, new Day5(example).PartOne());
+//        assertEquals(448, new Day5(input).PartOne());
+    }
+
+    @Test
+    public void testPartTwo() {
+//        assertEquals(4, new Day5(example).PartTwo());
+//        assertEquals(794, new Day5(input).PartTwo());
+    }
+}
