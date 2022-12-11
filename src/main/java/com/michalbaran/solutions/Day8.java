f86991a9db01dfaf4c30aa06e0f4a3b9b046c039
Michal Baran <michal.baran.dev@gmail.com>
2022-12-11 01:37:51 +0100

    Day 7 - Template

---
+++ src/main/java/com/michalbaran/solutions/Day8.java
@@ -0,0 +1,17 @@
+package com.michalbaran.solutions;
+
+public class Day8 extends Day {
+    public Day8(String filename) {
+        super(filename);
+    }
+
+    @Override
+    public Integer PartOne() {
+        return 0;
+    }
+
+    @Override
+    public <T> T PartTwo() {
+        return null;
+    }
+}
---
+++ src/test/java/com/michalbaran/solutions/Day8Test.java
@@ -0,0 +1,23 @@
+package com.michalbaran.solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day8Test {
+    String example = "/src/test/resources/day8_example.txt";
+    String input = "/src/main/resources/day8.txt";
+
+    @Test
+    public void testPartOne() {
+        assertEquals(21, new Day8(example).PartOne());
+//        assertEquals(1770595, new Day8(input).PartOne());
+    }
+
+    @Test
+    public void testPartTwo() {
+//        assertEquals(24933642, new Day8(example).PartTwo());
+//        assertEquals(2195372, new Day8(input).PartTwo());
+
+    }
+}
